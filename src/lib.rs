25d8170a19e8ab14124c3e56f1ae9e35f7482742
Corin Lawson <corin@responsight.com>
2019-06-13 15:26:51 +1000

    Use Display trait to format ExitStatus
    Prior to this change, ExitStatus would print errors to stderr using
    `"{:?}"` as the format string.  This allowed no flexibility to choose
    between `"{:?}"` and `"{}"`.
    
    This change introduces DebugExitStatus and changes ExitStatus to use
    `"{}"` whereas DebugExitStatus will use the old behaviour of ExitStatus.
    The default type parameter is also changed to maintain the old
    behaviour.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 #![feature(process_exitcode_placeholder)]
 #![feature(specialization)]
 
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 use std::ops::Try;
 use std::process::{ExitCode, Termination};
 
@@ -12,25 +12,51 @@ pub struct ExitStatus {
     pub error: String,
 }
 
-impl<T: Debug> From<T> for ExitStatus {
+impl<T: Display> From<T> for ExitStatus {
     default fn from(t: T) -> ExitStatus {
         ExitStatus {
             status_code: ExitCode::FAILURE.report(),
-            error: format!("{:?}", t),
+            error: format!("{}", t),
         }
     }
 }
 
-impl<T: Into<i32> + Debug> From<T> for ExitStatus {
+impl<T: Into<i32> + Display> From<T> for ExitStatus {
     default fn from(t: T) -> ExitStatus {
         ExitStatus {
-            error: format!("{:?}", t),
+            error: format!("{}", t),
             status_code: t.into(),
         }
     }
 }
 
-pub enum Exit<T = ExitStatus> {
+pub struct DebugExitStatus(ExitStatus);
+
+impl From<DebugExitStatus> for ExitStatus {
+    fn from(status: DebugExitStatus) -> Self {
+        status.0
+    }
+}
+
+impl<T: Debug> From<T> for DebugExitStatus {
+    default fn from(t: T) -> DebugExitStatus {
+        DebugExitStatus(ExitStatus {
+            status_code: ExitCode::FAILURE.report(),
+            error: format!("{:?}", t),
+        })
+    }
+}
+
+impl<T: Into<i32> + Debug> From<T> for DebugExitStatus {
+    default fn from(t: T) -> DebugExitStatus {
+        DebugExitStatus(ExitStatus {
+            error: format!("{:?}", t),
+            status_code: t.into(),
+        })
+    }
+}
+
+pub enum Exit<T = DebugExitStatus> {
     Ok,
     Err(T),
 }
